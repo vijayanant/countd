3ec06f4edb2a55c29aa87bec3bb9b1f8c892731b
Vijay Ananth <vijay.hassan@gmail.com>
2025-02-21 01:04:54 +0530

    feat: Integrate tracing with Bunyan formatter and tracing-appender for log rotation

--- src/core/counter/mod.rs
+++ src/core/counter/mod.rs
@@ -1,16 +1,19 @@
-#[derive(Debug, Clone, PartialEq, Eq)] // Add useful traits
+use tracing::instrument;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Counter {
     pub value: u64,
 }
 
 impl Counter {
     pub fn new(initial_value: u64) -> Self {
+        tracing::info!("Creating a new counter with initial value: {}", initial_value);
         Self { value: initial_value }
     }
 
+    #[instrument(name = "increment_counter", level = "debug")]
     pub fn increment(&mut self) {
         self.value += 1;
+        tracing::info!("Counter incremented to: {}", self.value);
     }
 }
-
-
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,36 @@
 mod core;
 mod tests;
 
-use crate::core::counter;
+use core::counter::Counter;
+
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter };
+use tracing_bunyan_formatter::{JsonStorageLayer, BunyanFormattingLayer};
 
 fn main() {
-    println!("Hello, world!");
+    let app_name = concat!(env!("CARGO_PKG_NAME"), "-", env!("CARGO_PKG_VERSION")).to_string();
+
+    let env_filter = EnvFilter::try_from_default_env()
+        .or_else(|_| EnvFilter::try_new("info"))
+        .unwrap();
+
+    let file_appender = tracing_appender::rolling::daily("./logs", "trace.log");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+
+    let bunyan_formatting_layer = BunyanFormattingLayer::new(app_name, non_blocking);
+    //let fmt_layer = fmt::layer().with_writer(move || non_blocking.clone());
+
+    if let Err(e) = tracing_subscriber::registry()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(bunyan_formatting_layer) // Remove this line if you don't need file output
+        .try_init()
+    {
+        eprintln!("Error initializing tracing: {}", e);
+        return;
+    }
+
+    let mut counter = Counter::new(0);
+    counter.increment();
+    counter.increment();
 }
+
