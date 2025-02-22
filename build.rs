7dee1ceedd40420e7f70410baaf8c07698b223ed
Vijay Ananth <vijay.hassan@gmail.com>
2025-02-22 15:42:17 +0530

    fix: compile proto definitions and generate gRPC code
    - Removed the `(rustproto.carllerche_bytes_for_bytes_all)` option from `eraftpb.proto`.
    - Added wrapper messages in `raft.proto` for gRPC compatibility.
    - Configured `build.rs` to correctly compile proto definitions.
    - Created `rpc` module and included generated code.

---
+++ build.rs
@@ -0,0 +1,10 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let proto_files = &["src/core/raft/proto/raft.proto"];
+
+    tonic_build::configure()
+        .build_server(true)
+        .compile_protos(proto_files, &["src/core/raft/proto"])?;
+
+    Ok(())
+}
+
---
+++ src/core/raft/rpc.rs
@@ -0,0 +1,3 @@
+pub mod proto {
+    tonic::include_proto!("raft"); // Replace "raft" with your package name
+}
